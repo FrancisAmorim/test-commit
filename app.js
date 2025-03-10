5006ef471efb8bc4de5d82430dd760c0c6459210
FrancisAmorim <francis.estudos@gmail.com>
2025-03-10 13:36:29 -0300

    update: utf-8

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200,{'Content-Type':'text/plain'});
-    res.end('Olá, Mundo!')
+    res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
+    res.end('Olá, mundo!');
 });
 
 const PORT = 3000;
-server.listen(PORT,() => {
-    console.log(`Servidor rodando em http:;//localhost:${PORT}`);
+server.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
-
